87b5c9ff6b1643777d9f2185ed5b1396350e0b83
TaiheiYamabe <91378148+Tyamabe@users.noreply.github.com>
2024-04-18 19:21:26 +0900

    カレンダー表示

--- src/main/java/com/dmm/task/controller/CalendarController.java
+++ src/main/java/com/dmm/task/controller/CalendarController.java
@@ -2,15 +2,21 @@ package com.dmm.task.controller;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+import org.springframework.scheduling.config.Task;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
 public class CalendarController {
     @GetMapping("/main")
     public String showCalendar(Model model) {
-        List<List<LocalDate>> monthList = new ArrayList<>();
+        System.out.println("###test###");
+    	List<List<LocalDate>> monthList = new ArrayList<>();
         LocalDate today = LocalDate.now();
         LocalDate firstDayOfMonth = today.withDayOfMonth(1);
         int daysFromSunday = firstDayOfMonth.getDayOfWeek().getValue() % 7;
@@ -21,16 +27,16 @@ public class CalendarController {
             List<LocalDate> weekRow = new ArrayList<>();
             for (int i = 0; i < 7; i++) {
                 weekRow.add(currentDay);
-                currentDay = currentDay.plusDays(1);
+                currentDay = currentDay.plusDays(1); //
             }
             monthList.add(weekRow);
         }
 
         model.addAttribute("matrix", monthList);
-        model.addAttribute("month", today.getMonth());
         model.addAttribute("prev", today.minusMonths(1));
         model.addAttribute("next", today.plusMonths(1));
-//hoge
+        Map<LocalDate, List<Task>> tasks = new HashMap<>();
+        model.addAttribute("tasks", tasks);
         return "main";
     }
 }
